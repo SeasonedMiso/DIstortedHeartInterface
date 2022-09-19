302e29e70fc8bfc7f90556f81216720dd1f31736
Zak <zakwrn@gmail.com>
2022-09-19 15:41:17 +0200

    Inport

--- arduinoShit/serialBlinkTest/serialBlinkTest.ino
+++ arduinoShit/serialBlinkTest/serialBlinkTest.ino
@@ -1,32 +1,84 @@
+
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 byte freq;
 String inString;
+unsigned long last = millis();
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); 
+  
+  lcd.begin(16, 2);
+  lcd.print(".");
+  lcd.setCursor(0,1);
+  lcd.print("");
+  delay(500);
+  lcd.begin(16, 2);
+  lcd.print("..");
+  lcd.setCursor(0,1);
+  lcd.print("");
+  delay(500);
+  lcd.begin(16, 2);
+  lcd.print(".");
+  lcd.setCursor(0,1);
+  lcd.print("");
+  delay(500);
+  lcd.begin(16, 2);
+  lcd.print("...");
+  lcd.setCursor(0,1);
+  lcd.print("");
+  delay(500);
   // start serial port at 9600 bps:
   Serial.begin(9600);
   while (!Serial) {
     ;
   }
-  pinMode(LED_BUILTIN, OUTPUT);
+  
   // Serial.println("test\n");
   // Serial.write("test\n");
+
+
+  
   freq = 15;
+  lcd.begin(16, 2);
+  lcd.print("initArd");
+  lcd.setCursor(0,1);
+  lcd.print("");
+  
 }
 
 void loop() {
-  while(Serial.available() == 0){
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500/freq);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(500/freq);                       // wait for a second
-  }
+  // while(Serial.available() == 0){
+  //   // digitalWrite(LED_BUILTIN, LOW); 
+  //   // if(last<millis()-1000){
+  //   //   last = millis();
+  //   //   digitalWrite(LED_BUILTIN, HIGH);
+  //   // }
+  //   // if(last<millis()-500){
+  //   //   digitalWrite(LED_BUILTIN, LOW);
+  //   // }
+  //   // digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    
+  //   // delay(500/freq);                       // wait for a second
+  //   // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  //   // delay(500/freq);                       // wait for a second
+    
+  // }
     while(Serial.available() > 0){
+      digitalWrite(LED_BUILTIN, HIGH); 
       inString = Serial.readString();
-      Serial.println(inString);
-      Serial.write("sent message\n");
-      if(inString.toInt()>0&&inString.toInt()<20){
-        freq = inString.toInt();
-      }
+      // Serial.println(inString);
+      // Serial.write("sent message\n");
+      lcd.begin(16, 2);
+      lcd.print("received");
+      lcd.setCursor(0,1);
+      lcd.print("something");
+      // if(inString.toInt()>0&&inString.toInt()<20){
+      //   freq = inString.toInt();
+      // }
     }
     
 } 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,7 @@
 
 use serde::de::value::Error;
 use serialport::SerialPortType::UsbPort;
-use std::time::Duration;
+use std::{thread::sleep, time::Duration};
 
 #[tauri::command]
 fn greet(name: &str) -> () {
@@ -25,43 +25,46 @@ fn find_arduino() {
     // Listing available ports:
     let ports = serialport::available_ports().expect("No ports found!");
     for p in ports {
-        println!("{}", p.port_name);
-        arduino_port_name = match p.port_type {
-            UsbPort(info) => {
-                println!("{:#06x}", info.vid);
-                println!("{:#06x}", info.pid);
-                if info.vid == 0x2341 && info.pid == 0x0043 {
-                    p.port_name
-                } else {
-                    "".to_string()
+        // println!("{}", p.port_name);
+        if p.port_name.contains("cu") {
+            arduino_port_name = match p.port_type {
+                UsbPort(info) => {
+                    // println!("{:#06x}", info.vid);
+                    // println!("{:#06x}", info.pid);
+                    if info.vid == 0x2341 && info.pid == 0x0043 {
+                        p.port_name
+                    } else {
+                        "".to_string()
+                    }
                 }
+                _ => "".to_string(),
             }
-            _ => "".to_string(),
         }
     }
-    println!("{}", arduino_port_name);
-
     let port_result = serialport::new(arduino_port_name, 9600)
         .timeout(Duration::from_millis(10))
         .open();
     if let Err(_err) = port_result {
         println!("Arduino not found");
-        // return;
+        return;
     } else {
         let mut port = port_result.unwrap();
         println!("Arduino found!");
+        sleep(Duration::new(5, 0));
         let mut output = "This is a test. This is only a test.".as_bytes();
+        // port.write(output).expect("Write failed!");
+        output = "2\n".as_bytes();
         port.write(output).expect("Write failed!");
-        output = "2".as_bytes();
-        port.write(output).expect("Write failed!");
-        let mut serial_buf: Vec<u8> = vec![0; 32];
+        port.flush().unwrap();
+
+        // let mut serial_buf: Vec<u8> = vec![0; 32];
         // loop {
         //     // .expect("Found no data!");
         //     let read_result = port.read(serial_buf.as_mut_slice());
         //     if let Ok(_) = read_result {
         //         let read_string = String::from_utf8_lossy(&serial_buf).to_string();
         //         println!("{}", read_string);
-        //         // println!("{:?}", serial_buf);
+        //         println!("{:?}", serial_buf);
         //         break
         //     };
         // }
